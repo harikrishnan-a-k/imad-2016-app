fb9b8e5b591bce82a44b2b415f6f258934518d9e
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2016-09-25 19:49:40 +0530

    Update server.js
    edit for image extension

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/m.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'm.png'));
+app.get('/ui/m.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'm.jpg'));
 });
 
 
