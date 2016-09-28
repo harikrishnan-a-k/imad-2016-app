3f6b77fb357b4c8606b5bde1045830e5993a007f
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2016-09-28 11:58:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+var img=document.getElementById('mypic');
+var marginLeft=0;
+function moveimg(){
+    if(marginLeft>200)
+    { 
+        marginLeft+=5;
+    }
+    else{
+        marginLeft-=5;
+    }
+    
+    img.style.marginLeft=marginLeft + 'px'; 
+}
+body.onload=function(){
+    var  interval= setInterval(moveimg, 50 );
+};
