a1ab42b35c371ed4595ff7ac44e51208a0fd65e6
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2016-10-03 12:44:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,6 +131,15 @@ app.get('/ui/m.jpg', function (req, res) {
 app.get('/ui/main.js', function(req,res){
     res.sendFile(path.join(__dirname,'ui','main.js'));
 });
+var names= [];
+app.get('/send/:name',function(req,res){
+    var name=req.params.name;
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+    
+    
+});
 
 
 
