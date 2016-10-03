9d7843437a1e94d7347dc25e9d538732bbc7f179
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2016-10-03 10:26:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,15 @@ function moveimg(){
 img.onload=function(){
     var  interval= setInterval(moveimg, 50 );
 };
+var button= document.getElementById('counter');
+var conunter=0;
+button.onclick= function(){
+    //make request to the counter
+    
+    // capture the response and save it in a variable
+
+    //render the incremented variable in correct span
+    counter =counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
