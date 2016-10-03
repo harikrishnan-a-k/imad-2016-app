8be925645497ff41630aea040c0569f2d3e2842b
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2016-10-03 13:09:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,19 +42,41 @@ button.onclick= function(){
     };
 
 // name list handling
-var nmeInput=document.getElementById('name');
-var namee=nmeInput.value;
+
 var submit=document.getElementById('send');
 submit.onclick = function(){
     
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++)
-    {
-        list += '<li>'+names[i] +'</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    //make request to the counter
+    var request= new XMLHttpRequest();
+    // capture the response and save it in a variable
+        request.onreadystatechange = function(){
+            if(request.readyState===XMLHttpRequest.DONE){
+                if(request.status===200){
+                    var names=request.responseText;
+                    names=JSON.parse(names);
+                    
+                                    var list='';
+                    for(var i=0;i<names.length;i++)
+                    {
+                        list += '<li>'+names[i] +'</li>';
+                    }
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML=list;
+                    
+                    
+                }
+            }
+        };
+   //make request
+   
+    var nmeInput=document.getElementById('name');
+    var namee=nmeInput.value;
+   request.open('GET','http://harikrishnan-a-k.imad.hasura-app.io/send/namee',true);
+   request.send(null);
+
+    
+    
+    
 };
 
 
